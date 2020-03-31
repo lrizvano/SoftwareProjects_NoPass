4bc6d704763a612035a9f5c6c28d5371f20dae06
lrizvano <lrizvano@uncc.edu>
2020-03-31 19:34:48 -0400

    added home screen skeleton

--- app/src/main/java/com/example/softwareprojects_nopass/MainActivity.java
+++ app/src/main/java/com/example/softwareprojects_nopass/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.softwareprojects_nopass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etDestination;
+    EditText etTime;
+    Button bSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("NoPass");
+
+        bSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
