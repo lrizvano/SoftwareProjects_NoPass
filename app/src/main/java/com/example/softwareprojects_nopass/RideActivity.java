7ea4d709b0069acc9a2e94171c30ae5e3b1d4b3b
lrizvano <lrizvano@uncc.edu>
2020-04-02 15:14:10 -0400

    added ride activity information

--- app/src/main/java/com/example/softwareprojects_nopass/RideActivity.java
+++ app/src/main/java/com/example/softwareprojects_nopass/RideActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 public class RideActivity extends AppCompatActivity {
     TextView tvDriverName;
     TextView tvDriverCar;
+    TextView tvDestination;
+    TextView tvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,14 @@ public class RideActivity extends AppCompatActivity {
 
         tvDriverName = findViewById(R.id.tvDriverName);
         tvDriverCar = findViewById(R.id.tvDriverCar);
+        tvDestination = findViewById(R.id.tvDestination);
+        tvTime = findViewById(R.id.tvTime);
 
         Ride ride = (Ride)getIntent().getExtras().getParcelable(MainActivity.RIDE_KEY);
 
         tvDriverName.setText("Driver: " + ride.getDriver());
         tvDriverCar.setText("Car: " + ride.getCar());
+        tvDestination.setText("Time: " + ride.getTime());
+        tvTime.setText("Time: " + ride.getTime());
     }
 }
