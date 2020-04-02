c07ac56902487a141e7b20ca2a0732000c792807
lrizvano <lrizvano@uncc.edu>
2020-04-02 14:54:10 -0400

    created ride object

---
+++ app/src/main/java/com/example/softwareprojects_nopass/Ride.java
@@ -0,0 +1,12 @@
+package com.example.softwareprojects_nopass;
+
+public class Ride {
+    private String driver, car, destination, time;
+
+    public Ride(String destination, String time) {
+        this.driver = "Elon Musk";
+        this.car = "Tesla";
+        this.destination = destination;
+        this.time = time;
+    }
+}
