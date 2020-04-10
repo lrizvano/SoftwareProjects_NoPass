25f5f3a8a3cc9296c42cda630dc64d99499646e4
lrizvano <lrizvano@uncc.edu>
2020-04-09 20:43:04 -0400

    filled out payment activity

--- app/src/main/java/com/example/softwareprojects_nopass/PaymentActivity.java
+++ app/src/main/java/com/example/softwareprojects_nopass/PaymentActivity.java
@@ -3,13 +3,20 @@ package com.example.softwareprojects_nopass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PaymentActivity extends AppCompatActivity {
+    EditText etCardName, etCardNumber, etCardExpiration, etCardCVV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         setTitle("NoPass");
+
+        etCardName = findViewById(R.id.etCardName);
+        etCardNumber = findViewById(R.id.etCardNumber);
+        etCardExpiration = findViewById(R.id.etCardExpiration);
+        etCardCVV = findViewById(R.id.etCardCVV);
     }
 }
